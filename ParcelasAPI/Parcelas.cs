371c610491575a71a8b90c144c48dc8da1aeff65
Lucas <lucascalazans04@hotmail.com>
2020-12-01 11:23:07 -0300

    Criação das classes Funcoes e Parcelas e obtenção dos dados do usuário pela main

---
+++ ParcelasAPI/Parcelas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcelasAPI
+{
+    public class Parcelas
+    {
+       public static double debito; 
+       public static int quantidadeParcelas;
+       public static double correcao;
+        public double Correcao
+        {
+            get { return correcao; }
+            set { correcao = value; }
+        }
+        public double Debito
+        {
+            get { return debito; }
+            set { debito = value; }
+        }
+        public int QuantidadeParcelas
+        {
+            get { return quantidadeParcelas; }
+            set { quantidadeParcelas = value; }
+        }
+
+    }
+}
--- ParcelasAPI/Program.cs
+++ ParcelasAPI/Program.cs
@@ -12,9 +12,41 @@ namespace ParcelasAPI
     //testando commit 
     public class Program
     {
+        Parcelas parcela = new Parcelas();
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            string entradaQuantidadeParcelas;
+            string entradaDebito;
+
+            int quantidadeParcelas;
+            double debito;
+
+            Console.WriteLine("Digite o valor do débito: ");
+            entradaDebito = Console.ReadLine();
+
+            try
+            {
+                debito = Convert.ToDouble(entradaDebito);
+                Parcelas.debito = debito;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Valor inválido para o campo débito");
+            }
+            Console.WriteLine("Digite o número de parcelas: ");
+            entradaQuantidadeParcelas = Console.ReadLine();
+
+            try { 
+                quantidadeParcelas = Convert.ToInt32(entradaQuantidadeParcelas);
+                Parcelas.quantidadeParcelas = quantidadeParcelas;
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Valor inválido para número de parcelas");
+            }           
+            //Console.WriteLine(Parcelas.quantidadeParcelas);
+            //Console.WriteLine(Parcelas.debito);
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
